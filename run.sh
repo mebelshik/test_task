90d774d1cd684bac06ff66dbd9bacc2620330688
mebelshik <98887195+mebelshik@users.noreply.github.com>
2024-04-11 12:28:00 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Временная директория для клонирования репозитория
-TEMP_DIR=$(mktemp -d)
+TEMP_DIR=$(mktemp -d) || exit 1
 
 # Клонирование репозитория
-git clone https://github.com/mebelshik/test_task.git "$TEMP_DIR"
+git clone https://github.com/mebelshik/test_task.git "$TEMP_DIR" || exit 1
 
 # Переход в директорию проекта
-cd "$TEMP_DIR"
+cd "$TEMP_DIR" || exit 1
 
 # Запуск playbook Ansible
-ansible-playbook playbook.yml
+ansible-playbook playbook.yml || exit 1
 
 # Удаление временной директории (опционально)
 rm -rf "$TEMP_DIR"
